33fc35797ce35cedb8be22741f17396b48533e46
danielle <daniellehenry748@yahoo.com>
2021-06-16 00:31:42 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,67 @@
+function add(a,b){
+return a+b
+
+}
+
+function subtract(b,a){
+return b-a
+}
+
+function multiply(a,b){
+return a*b
+}
+
+function divide(a,b){
+return a/b
+}
+
+function increment (n) {
+    n ++
+    return n
+}
+
+function decrement (n){
+    n--
+    return n
+}
+
+function makeInt(n){
+    return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 
+ * + - addition
+ * - - subtracton
+ * * - multiplication
+ * / - division
+ * 
+ * ++ - increment
+ * -- - decrement
+ * += - add and set
+ * -=
+ * /=
+ * *=
+ *
+ */
